691ff60d9324798e0030a99b5346848246dfae0e
savisch <savisch@gmail.com>
2020-12-02 17:37:22 -0600

    Table/color variables, working "makeGrid" fxn

--- designs.js
+++ designs.js
@@ -1,10 +1,20 @@
+
+let table = document.getElementById('pixelCanvas');
 // Select color input
+let color = document.getElementById('colorPicker');
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    let height = document.getElementById('inputHeight').value;
+    let width = document.getElementById('inputWidth').value;
+    for(let i = 0; i < height; i ++){
+        let row = table.insertRow();
+        for(let j = 0; j < width; j ++){
+            let cell = row.insertCell();
+            cell.innerHTML = "";
+        }
+    }
 }
+
