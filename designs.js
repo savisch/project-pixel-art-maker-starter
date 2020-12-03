7a6d29fbdd6e968011bcfc7c5a28c3950b4e5797
savisch <savisch@gmail.com>
2020-12-02 21:19:21 -0600

    Add comments

--- designs.js
+++ designs.js
@@ -2,13 +2,18 @@
 // Select color input
 let color;
 let colorPick = document.getElementById('colorPicker');
+//listen for change in color selection from default color
 colorPick.addEventListener('change', function(){
     color = document.getElementById('colorPicker').value;
 });
 
 let submitButton = document.getElementById('submitButton');
+//listen for click on submit button
 submitButton.addEventListener('click', buttonAction);
 let count = 0;
+
+//calls make grid if first submit click
+//clears inner HTML and makes new grid if any addt'l clicks
 function buttonAction(){
     count ++;
     if(count === 1){
