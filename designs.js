2507d37c96d08f7ac8ebf413053115acfd21d235
savisch <savisch@gmail.com>
2020-12-02 20:20:23 -0600

    Add click to change cell color / listener for color choice

--- designs.js
+++ designs.js
@@ -1,20 +1,26 @@
 
-let table = document.getElementById('pixelCanvas');
 // Select color input
-let color = document.getElementById('colorPicker');
-// Select size input
+let color;
+let colorPick = document.getElementById('colorPicker');
+colorPick.addEventListener('change', function(){
+    color = document.getElementById('colorPicker').value;
+});
 
-// When size is submitted by the user, call makeGrid()
-
+//create grid from input height and width
+//add event listener on cells to change background-color
 function makeGrid() {
+    let table = document.getElementById('pixelCanvas');
     let height = document.getElementById('inputHeight').value;
     let width = document.getElementById('inputWidth').value;
+
     for(let i = 0; i < height; i ++){
         let row = table.insertRow();
         for(let j = 0; j < width; j ++){
             let cell = row.insertCell();
             cell.innerHTML = "";
-        }
-    }
-}
-
+            cell.addEventListener('click', function(){
+                cell.style.backgroundColor = color;
+            });
+        };
+    };
+};
